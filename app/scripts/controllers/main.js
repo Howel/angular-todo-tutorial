87c805249cc62ba23bb066842c975ae3ea1df25b
Jared Bydeley <jbydeley@gmail.com>
2013-06-22 19:47:13 -0400

    Now supports adding todos.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,10 @@
 angular.module('todoApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = ['Buy Milk', 'Eat Cookies', 'Have Party'];
+
+    $scope.addTodo = function() {
+		$scope.todos.push($scope.todo);
+		$scope.todo = '';
+    };
+
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -19,4 +19,12 @@ describe('Controller: MainCtrl', function () {
   it('should attach a list of todos to the scope', function () {
     expect(scope.todos.length).toBe(3);
   });
+
+  it('should add a todo when addTodo is called', function () {
+    scope.todo = 'Another Todo';
+    scope.addTodo();
+    expect(scope.todos.length).toBe(4);
+    expect(scope.todos.indexOf('Another Todo')).toBe(3);
+    expect(scope.todo).toBe('');
+  });
 });
