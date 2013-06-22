218cd55b2cc7163ecb856c93fe55499a7198c42c
Jared Bydeley <jbydeley@gmail.com>
2013-06-22 19:38:33 -0400

    Added Todos to main. Updated Tests to reflect changes.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,5 @@
 
 angular.module('todoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.todos = ['Buy Milk', 'Eat Cookies', 'Have Party'];
   });
