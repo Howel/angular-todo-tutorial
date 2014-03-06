e980f0e9a9edc8186cafeda80d2dfcf8ea94c02b
Jared Bydeley <jbydeley@gmail.com>
2014-03-06 16:27:49 -0500

    Updated to latest Angular and bootstrap. Updated UI

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('todoApp', ['LocalStorageModule'])
+angular.module('todoApp', ['ngRoute', 'LocalStorageModule'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
