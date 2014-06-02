32af3991c878071a9479aa408b40d8eea097449c
Jared Bydeley <jbydeley@gmail.com>
2014-06-02 18:57:41 -0400

    Latest components. Use IE as default since Chrome requires changes.

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['ChromeCanary'];
+browsers = ['IE'];
 
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 5000;
